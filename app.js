6c872a67c13d23a55494290dd56fd7dbf1a95eac
jeiiy.wang <jeiiy.wang@wingconn.com>
2018-11-28 10:45:08 +0800

    node ejs模板引擎框架搭建

---
+++ app.js
@@ -0,0 +1,46 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var ejs = require('ejs');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var hahaRouter = require('./routes/haha');
+
+var app = express();
+
+// view engine setup
+app.engine("html", ejs.__express); //如果views文件夹里需要渲染.html文件 此项设置需要 否则注释掉
+app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'ejs'); //如果views文件夹里需要渲染.ejs文件 此项设置需要 否则注释掉
+app.set('view engine', 'html');//如果views文件夹里需要渲染.html文件 此项设置需要 否则注释掉
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/haha',hahaRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ readme.txt
@@ -0,0 +1,12 @@
+安装脚手架
+npm install express-generator -g
+创建项目
+express myapp
+进入项目
+cd myapp 
+安装依赖
+npm i
+启动项目
+npm start
+
+express中文官网  http://www.expressjs.com.cn/starter/generator.html
